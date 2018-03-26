c1935c848899f00d9e24da068e8e0a9844865412
Adeyinka Philip Apata <phil2dcore@gmail.com>
2018-03-26 15:54:51 +0100

    latest update

--- src/layouts/nav.js
+++
@@ -1,8 +0,0 @@
-let navButton = document.querySelector('button[aria-expanded]');
-
-function toggleNav({ target }) {
-  let expanded = target.getAttribute('aria-expanded') === true || false;
-  navButton.setAttribute('aria-expanded', !expanded)
-};
-
-navButton.addEventListener('click', toggleNav);
